20c97ff8be44a6d6752ee76471f8cbdfb0c514bd
jesusbrito <udvuvvdu29>
2022-01-28 16:13:07 -0600

    Initial commit from Create Next App

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-import '../styles/globals.scss'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

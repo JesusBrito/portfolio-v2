4c57383ef8d69d6a313fcc2fb67b5ec26460b006
jesusbrito <udvuvvdu29>
2022-02-10 01:15:02 -0600

    ajustes en imagenes

--- components/ModalProject/ModalProject.jsx
+++ components/ModalProject/ModalProject.jsx
@@ -1,13 +1,15 @@
 import {Button, Modal} from "react-bootstrap";
 import Image from 'next/image'
-
+import style from './ModalProject.module.scss'
 const ModalProject = ({show, setShow, project}) => {
     return <Modal
         show={show}
         onHide={() => setShow(false)}
         size="lg">
         <Modal.Header closeButton>
-            <Modal.Title>{project?.name}</Modal.Title>
+            <Modal.Title>
+                <h1 className={style.title}>{project?.name}</h1>
+            </Modal.Title>
         </Modal.Header>
         <Modal.Body>
             <Image
@@ -16,7 +18,7 @@ const ModalProject = ({show, setShow, project}) => {
                 width="1920px"
                 height="1080px"
                 layout="responsive"/>
-            {project?.largeDescription}
+            <p className={style.text}>{project?.largeDescription}</p>
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={() => setShow(false)}>
